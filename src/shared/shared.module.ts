e81e0192464c328ca0de6b4f97291c141d6737e5
Jérémy PAYET <jpayet@diginamic-formation.fr>
2022-06-23 18:06:37 +0400

    Like-Hate

---
+++ src/shared/shared.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LikeHateComponent } from '../shared/components/like-hate/like-hate.component';
+
+
+
+@NgModule({
+  declarations: [
+    LikeHateComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    LikeHateComponent
+  ],
+  bootstrap: [
+    LikeHateComponent
+  ]
+})
+export class SharedModule { }
