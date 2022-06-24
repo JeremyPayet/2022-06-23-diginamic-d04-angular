e5d797856d2bd80efd7850c9238702276b5b9316
Jérémy PAYET <jpayet@diginamic-formation.fr>
2022-06-24 13:28:13 +0400

    Limite Score

--- src/shared/components/colleague/colleague.component.ts
+++ src/shared/components/colleague/colleague.component.ts
@@ -1,5 +1,7 @@
+import { LikeHateComponent } from './../like-hate/like-hate.component';
+import { LikeHate } from './../../../models/like-hate';
 import { Colleague } from './../../../models/colleague';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'tc-colleague',
@@ -9,15 +11,42 @@ import { Component, Input, OnInit } from '@angular/core';
 export class ColleagueComponent implements OnInit {
 
 
-  @Input() colleague :Colleague = {
+  @Input() colleague: Colleague = {
     pseudo: "Pseudo",
-    score: "+ 1000",
-    photo: "#"
+    score: 1000,
+    photo: "https://images.assetsdelivery.com/compings_v2/tanyadanuta/tanyadanuta1910/tanyadanuta191000003.jpg"
   };
+  @Output() disabledL?: boolean;
+  @Output() disabledH?: boolean;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  changeValue(val: LikeHate): void {
+    if (val == LikeHate.LIKE) {
+      this.colleague.score = this.colleague.score + 1;
+    } else {
+      this.colleague.score = this.colleague.score - 1;
+    }
+    this.disableLike();
+    this.disableHate();
+  }
+
+  disableLike(): boolean {
+    if (this.colleague.score >= 1000) {
+      return true
+    } else {
+      return false;
+    }
+  }
+  disableHate(): boolean {
+    if (this.colleague.score <= -1000) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
 }
--- src/shared/components/like-hate/like-hate.component.ts
+++ src/shared/components/like-hate/like-hate.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { ColleagueComponent } from './../colleague/colleague.component';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { LikeHate } from 'src/models/like-hate';
 
 @Component({
   selector: 'tc-like-hate',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LikeHateComponent implements OnInit {
 
+  @Input() disabledL?: boolean;
+  @Input() disabledH?: boolean;
+
+  @Output() change: EventEmitter<LikeHate> = new EventEmitter<LikeHate>();
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  like() {
+    this.change.emit(LikeHate.LIKE);
+  }
+  hate() {
+    this.change.emit(LikeHate.HATE);
+  }
 }
