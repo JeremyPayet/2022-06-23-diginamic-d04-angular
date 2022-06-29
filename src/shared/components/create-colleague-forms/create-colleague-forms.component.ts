fc544c3bed90fec505986626fc9e3607cf6dfced
Jérémy PAYET <jpayet@diginamic-formation.fr>
2022-06-29 13:37:18 +0400

    Ajout collegue form

--- src/shared/components/create-colleague-forms/create-colleague-forms.component.ts
+++ src/shared/components/create-colleague-forms/create-colleague-forms.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpHeaders } from '@angular/common/http';
+import { Colleague } from './../../../models/colleague';
+import { HttpClient } from '@angular/common/http';
+import { Component, Input, OnInit, Output } from '@angular/core';
 
 class MonModel {
   pseudo: string = "";
@@ -6,7 +9,7 @@ class MonModel {
   prenom: string = "";
   photo_url: string = "";
 }
-
+const link = "https://colleagues-app.herokuapp.com/api/v2/colleagues"
 @Component({
   selector: 'tc-create-colleague-forms',
   templateUrl: './create-colleague-forms.component.html',
@@ -15,10 +18,32 @@ class MonModel {
 export class CreateColleagueFormsComponent implements OnInit {
 
   monModel = new MonModel();
+  collegue!: Partial<Colleague>;
 
-  constructor() { }
+  @Input() @Output() colleague_list!: Colleague[];
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      "Content-Type": "application/json"
+    })
+  };
+
+  constructor(private client: HttpClient) {
+  }
 
   ngOnInit(): void {
+
   }
+  valider() {
+    return this.client.post<Partial<Colleague>>(link, {
+      "pseudo": this.monModel.pseudo,
+      "last": this.monModel.nom,
+      "first": this.monModel.prenom,
+      "photo": this.monModel.photo_url
+    }).subscribe(colleague => {
+      this.collegue = colleague
+    })
+  }
+
 
 }
